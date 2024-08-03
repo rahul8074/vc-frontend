e9ba14b83ea3d39c7c4689f89db08d17f30b1f4e
Rahul Rajput <66771708+rahul8074@users.noreply.github.com>
2024-08-03 15:45:21 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'https://vc-backend-oih5.onrender.com' // Replace with your backend URL
-    }
-  }
-});
+  plugins: [react()],
+})
